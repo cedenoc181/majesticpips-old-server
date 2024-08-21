c4d2d3645d33f7435b2f10f6d593a1fe246c80b4
Christian <cedenoc181@gmail.com>
2024-08-21 16:34:50 -0400

    asdded arguments to puppeteer launch function for heroku to luanch puppeteer

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ app.get('/scrape-data', async (req, res) => {
     } 
 
     // Scrape new data
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    
     const page = await browser.newPage();
     await page.goto('https://markets.ft.com/data/currencies', { waitUntil: 'networkidle2' });
 
